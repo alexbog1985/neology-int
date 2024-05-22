463d91bdec7858dec776d8fbc0d7260d3368168a
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-05-22 08:20:19 +0300

    add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Stack
+from main import Stack, check_balance
 
 
 class TestMain(unittest.TestCase):
@@ -10,6 +10,26 @@ class TestMain(unittest.TestCase):
         self.assertEqual(self.stack.push(1), [1])
         self.assertEqual(self.stack.push(2), [1, 2])
         self.assertEqual(self.stack.push(3), [1, 2, 3])
+
+    def test_check_balance(self):
+        for i, string in enumerate([
+            '(((([{}]))))',
+            '[([])((([[[]]])))]{()}',
+            '{{[()]}}'
+        ]):
+            with self.subTest(i=i):
+                actual = check_balance(string)
+                self.assertEqual(actual, True)
+
+    def test_check_balance_fail(self):
+        for i, string in enumerate([
+            '}{}',
+            '{{[(])]}}'
+            '[[{())}]'
+        ]):
+            with self.subTest(i=i):
+                actual = check_balance(string)
+                self.assertEqual(actual, False)
 
 
 if __name__ == '__main__':
